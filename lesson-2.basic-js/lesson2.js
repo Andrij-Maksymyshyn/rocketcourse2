d11f6175678cce6feb35ce86ff0733e1d5f41fe3
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-09-26 23:25:52 +0300

    done

---
+++ lesson-2.basic-js/lesson2.js
@@ -0,0 +1,103 @@
+//Task2.1
+
+const arr1 = [1, "Hi", true, "My name is Andrij", 7];
+
+// for (let i = 0; i < arr1.length; i += 1) {
+//     if (typeof arr1[i] === 'number') {
+//         console.log(arr1[i]);
+//     }};
+
+//Task2.2
+
+    // for (let i = 0; i < arr1.length; i += 1) {
+    // if (arr1[i].length >= 4) {
+    //     console.log(arr1[i]);
+    // }};
+
+//Task2.3
+
+//  for (let i = 0; i < arr1.length; i += 1) {
+//     if (typeof arr1[i] === 'boolean') {
+//         console.log(arr1[i]);
+//     }};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Task3.1
+
+// for (let i = 1; i <= 100; i += 1) {
+//     console.log(i)
+// };
+
+//Task3.2
+
+// for (let i = 1; i <= 100; i += 1) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// };
+
+//Task3.2
+
+// for (let i = 1; i <= 100; i += 1) {
+//     if (i % 3 === 0) {
+//         console.log(i)
+//     }
+// };
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Task4.1
+
+const arr2 = ['js', 'css', 'jq'];
+
+// let firstEl = arr2.shift();
+// console.log('firstEl:', firstEl);
+// console.log(arr2);
+// let lastEl = arr2.pop();
+// console.log('LastEl:', lastEl);
+// console.log(arr2);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Task5.1
+
+const arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let oddNum = arr3.map(el => {
+//     if (el % 2 === 0) {
+//         console.log(el);
+//     };
+// });
+
+//Task5.2
+
+// let newArr3 = [];
+// for (let i = 0; i < arr3.length; i += 1) {
+//     newArr3.push(arr3[i]);
+// };
+// console.log(newArr3);
+// console.log(arr3);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Task6.1
+
+const arr4 = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = 0;
+// while (i < arr4.length) {
+//     console.log(arr4[i]);
+//     i++;
+// };
+
+
+
+
+
